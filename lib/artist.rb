1b2fadc710a55b5e79b7e64e0c2a721a9c7f9246
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-02-03 05:41:52 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,10 +6,25 @@ class Artist
 
   def initialize(name)
     @name = name
-    @@all < self
+    @@all << self
   end
 
   def self.all
     @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    artist = Artist.new(name)
+    artist.save
+    artist
+
   end
 end
--- lib/genre.rb
+++ lib/genre.rb
@@ -6,10 +6,25 @@ class Genre
 
   def initialize(name)
     @name = name
-    @@all < self
+    @@all << self
   end
 
   def self.all
     @@all
   end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    genre = Genre.new(name)
+    genre.save
+    genre
+  end
+
 end
