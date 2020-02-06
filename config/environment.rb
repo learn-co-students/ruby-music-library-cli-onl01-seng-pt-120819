42e7e5533de9e1bdd5900906cbe25e08988260ef
kowalix007 <kowalix007@gmail.com>
2020-02-06 00:16:04 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -6,51 +6,6 @@ end
 
 require_all 'lib'
 
-class Song 
-  attr_accessor :artist, :name, :genre
-  
-  
-  @@all = []
-  
-  def initialize (name, artist = nil, genre = nil)
-   @name = name
-   self.artist = artist if artist
-   self.genre = genre if genre
-   @genre
-  end 
-  
-  def self.all
-    @@all
-  end
-  
-  def self.destroy_all 
-    @@all.clear
-  end 
-  
-  def save
-    @@all << self
-  end 
-  
-  def self.create(name)
-    song = Song.new(name)
-    song.save
-    song
-  end 
-  
-  def genre
-    @genre
-  end 
-  
-  def artist=(artist)
-    @artist = artist
-    artist.add_song(self)
-  end
-  
-  def genre=(genre)
-    @genre = genre
-    genre.songs << self unless genre.songs.include? self
-  end
-end
 
 class Artist 
   attr_accessor :name
