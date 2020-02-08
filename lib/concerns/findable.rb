a14205f83ef01a3b7b2221e31d42a36e9a620cf1
kowalix007 <kowalix007@gmail.com>
2020-02-08 17:22:44 +0000

    Done.

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -3,8 +3,8 @@ module Findable
      def find_by_name(name)
        all.detect{|s| s.name == name}
      end
-      def find_or_create_by_name(name)
-      find_by_name(name) || create(name)
-  end
+  #     def find_or_create_by_name(name)
+  #     find_by_name(name) || create(name)
+  # end
   end
  end
